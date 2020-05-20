af3570abaf5668a342f22273a9f0ce14fbd97a4f
Andreas Heumaier <andreas.heumaier@microsoft.com>
2020-05-20 22:32:43 +0200

    Fix bats errrors

--- scripts/setup_ubuntu_desktop.sh
+++ scripts/setup_ubuntu_desktop.sh
@@ -3,7 +3,7 @@
 #  Perparing the linux system for  operating
 #  simulation VTD components
 #
-#   Usage: sudo ./setup_linux_refactor.sh
+#   Usage: sudo ./setup_ubuntu_desktop.sh
 #
 #
 # === Break on the first Error ===
@@ -13,15 +13,9 @@ if [ "${DEBUG}" ]; then
     set -o xtrace # Similar to -v, but expands commands, same as "set -x"
 fi
 
-# === Global vars we need for conan to run ===
-export CONAN_USER_HOME="/opt/conan"
-
-ORIG_PATH=$(cut -d '=' -f2 /etc/environment)
-export ORIG_PATH
-
 # === Cleanup actions ===
 function finish() {
-    # sudo /usr/sbin/waagent -force -deprovision+user && export HISTSIZE=0 && sync
+    sudo /usr/sbin/waagent -force -deprovision+user && export HISTSIZE=0 && sync
     rm -rf "$TMP_DIR"
 }
 # === Enforce clean-up on any circumstances ===
@@ -51,71 +45,58 @@ install_nvidia_drivers() {
     sudo apt install -y nvidia-driver-415
     # Installing CUDA drivers. Must be resolved in a separate step because cuda-drivers cannot properly resolve the dependency to the display drivers.
     sudo apt install -y cuda-drivers
+    sudo nvidia-xconfig  --virtual="1920x1080"  # We will need it later for x11vnc
 }
 
-# === Configure xrdp environment ===
-setup_xrdp() {
-    sudo sed -e 's/^new_cursors=true/new_cursors=false/g' -i /etc/xrdp/xrdp.ini
-    sudo systemctl enable xrdp
-}
+# === Configure x11vnc environment ===
+setup_x11vnc() {
+    sudo tee "/etc/systemd/system/x11vnc.service" >/dev/null <<'EOF'
+[Unit]
+Description=x11vnc VNC Server for X11
+Requires=display-manager.service
+After=display-manager.service
 
-# === Installing Conan ===
-setup_conan() {
-    sudo pip3 install conan
-    sudo mkdir "${CONAN_USER_HOME}"
-    sudo chmod -R a+w "${CONAN_USER_HOME}"
-    conan config install /tmp/conan_config # Packer creates /tmp/conan_config
-    conan config set storage.path="${CONAN_USER_HOME}"
-    conan user "${USER_DEVSTACK}" -r adapmt7c -p "${PASS_DEVSTACK}"
+[Service]
+Type=forking
+ExecStartPre=/bin/bash -c "/bin/systemctl set-environment SDDMXAUTH=$(/usr/bin/find /var/run/sddm/ -type f)"
+ExecStart=/usr/bin/x11vnc -display :0 -auth ${SDDMXAUTH} -forever -shared -bg -o /var/log/x11vnc.log -rfbauth /etc/x11vnc.passwd -xkb -norc -noxrecord -noxdamage -nomodtweak
+ExecStop=/usr/bin/killall x11vnc
+Restart=on-failure
+RestartSec=2
 
-}
+[Install]
+WantedBy=graphical.target
 
-# === Installing VTD Procudure ===
-install_vtd() {
-    setup_conan
-    conan install "${VERSION_TOOLCHAIN}" -g virtualenv -r adapmt7c
-    sed \
-        -e '/CONAN_OLD/d' \
-        -e '/ADS2LICENSE=/d' \
-        -e "s%\${PATH+:\$PATH}%${ORIG_PATH+:$ORIG_PATH}%" \
-        -e "s%\${LD_LIBRARY_PATH+:\$LD_LIBRARY_PATH}%${LD_LIBARY_PATH+:$LD_LIBRARY_PATH}%" \
-        -e 's/"//g' \
-        -e "\$CONAN_USER_HOME=/opt/conan" environment.sh.env | sudo tee /etc/environment
+EOF
+    sudo x11vnc -storepasswd "${VNC_PASS}" /etc/x11vnc.passwd
+    sudo systemctl enable x11vnc.service
 }
 
 # === MAIN PROCEDURE ===
 run_main() {
     declare -ra required_env_vars=(
-        "${USER_DEVSTACK}"
-        "${PASS_DEVSTACK}"
-        "${VERSION_TOOLCHAIN}"
-        "${LD_LIBRARY_PATH}"
-        "${NVIDIA_PKG}"
-        "${CUDA_PKG}"
-        "${NVIDIA_REPO}"
+        "${VNC_PASS}"
+        "${NVIDIA_PKG:-"nvidia-diag-driver-local-repo-ubuntu1804-415.25_1.0-1_amd64.deb"}"
+        "${CUDA_PKG:-"cuda-repo-ubuntu1804_10.0.130-1_amd64.deb"}"
+        "${NVIDIA_REPO-:"http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64"}"
     )
     for var in "${required_env_vars[@]}"; do
-        if [ -z "${var}" ]; then
+        if [ -z "${var}" ]; then    
             var_name=("${!var@}")
             echo "Empty required env var found: ${var_name[*]}. ABORT"
             exit 1
         fi
     done
 
-    local -r enable_nvidia=false
     declare -ar PACKAGES=(
-        wget curl
-        # Install GUI
-        kde-plasma-desktop xrdp
-        # VTD dependencies
-        xterm freeglut3 openssh-server nfs-common mesa-utils xfonts-75dpi libusb-0.1-4
-        libqtgui4 libqt4-dev libgsm1 libpulse0 libcrystalhd3 libmpg123-0 libdvdread4
+        wget curl kde-plasma-desktop x11vnc
+        xterm freeglut3 openssh-server  mesa-utils xfonts-75dpi libusb-0.1-4
+        libgsm1 libpulse0 libcrystalhd3 libmpg123-0 libdvdread4
         libxvidcore4 libaa1 libfaad2 libxss1 libopencore-amrnb0 libopencore-amrwb0
         libspeex1 libjack-jackd2-0 libdv4 libdca0 libtheora0 libxvmc1 libbs2b0 libmp3lame0
         libmad0 liblircclient0 libsmbclient libsdl1.2debian libtwolame0 libenca0
-        # Conan needs pip
-        python3-pip
     )
+    
     declare TMP_DIR
     TMP_DIR=$(mktemp -d -t tmp.XXXXXXXXXX || exit 1)
     declare -r TMP_DIR
@@ -123,12 +104,8 @@ run_main() {
     install_system_packages
     install_nvidia_repos
     install_cuda_repos
-
-    if [ $enable_nvidia ]; then
-        install_nvidia_drivers
-    fi
-    setup_xrdp
-    install_vtd
+    install_nvidia_drivers
+    setup_x11vnc
 }
 
 #  Be able to run this one either as standalone or import as lib
--- scripts/test/setup_ubuntu_desktop.bats
+++ scripts/test/setup_ubuntu_desktop.bats
@@ -13,28 +13,18 @@ function teardown() {
     docker stop $DOCKER_SUT_ID && docker rm $DOCKER_SUT_ID
 }
 
-@test "test CONAN_USER_HOME is set" {
-    source ${profile_script}
-    run echo $CONAN_USER_HOME
-    assert_output "/opt/conan"
-}
-
-@test "test ORIG_PATH is set" {
-    source ${profile_script}
-    run echo $ORIG_PATH
-    assert_output "\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games\""
-}
 @test "test install_system_packages() should install provided packes" {
-    skip
+    # skip
     source ${profile_script}
     function sudo() { docker_mock "${*}";  }
     export -f sudo 
-    declare -ar PACKAGES=( wget curl gnupg2 xrdp libusb-0.1-4 libxvidcore4 libaa1 libfaad2 libxss1 libopencore-amrnb0 libopencore-amrwb0 )
+    declare -ar PACKAGES=( wget curl gnupg2 x11vnc libusb-0.1-4 libxvidcore4 libaa1 libfaad2 libxss1 libopencore-amrnb0 libopencore-amrwb0 )
     run install_system_packages
     assert_success
 
 }
 @test "test install_nvidia_repos() should install nvidia_repos" {
+    # skip
     NVIDIA_PKG="nvidia-diag-driver-local-repo-ubuntu1804-415.25_1.0-1_amd64.deb"
     NVIDIA_REPO="http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64"
     TMP_DIR="/tmp"
@@ -50,6 +40,7 @@ function teardown() {
 
 }
 @test "test install_cuda_repos() should install cuda_repos" {
+    # skip
     CUDA_PKG="cuda-repo-ubuntu1804_10.0.130-1_amd64.deb"
     NVIDIA_REPO="http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64"
     TMP_DIR="/tmp"
@@ -64,7 +55,25 @@ function teardown() {
     assert_success
 
 }
+@test "test setup_x11vnc() should be properly configured" {
+    skip
+    VNC_PASS=$(openssl rand -base64 12)
+    TMP_DIR="/tmp"
+    source ${profile_script}
+    function systemctl() { echo "This is running ${*}";  }
+    export -f systemctl
+    function sudo() { docker_mock "${*}";  }
+    export -f sudo 
+    declare -ar PACKAGES=( wget curl gnupg2 x11vnc )
+    run install_system_packages 
+    run setup_x11vnc
+    assert_success
+    run cat /etc/x11vnc.passwd
+    assert_output ${VNC_PASS}
+}
+
 @test "test run_main should fail on missin env var" {
+#   skip
     NVIDIA_PKG="nvidia-diag-driver-local-repo-ubuntu1804-415.25_1.0-1_amd64.deb"
     NVIDIA_REPO="http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64"
     CUDA_PKG="cuda-repo-ubuntu1804_10.0.130-1_amd64.deb"
@@ -76,13 +85,12 @@ function teardown() {
     assert_output "Empty required env var found: var var_name. ABORT"
 }
 @test "test run_main should be successfull" {
+#   skip
     source ${profile_script}
+    VNC_PASS=$(openssl rand -base64 12)
     NVIDIA_PKG="nvidia-diag-driver-local-repo-ubuntu1804-415.25_1.0-1_amd64.deb"
     NVIDIA_REPO="http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64"
     CUDA_PKG="cuda-repo-ubuntu1804_10.0.130-1_amd64.deb"
-    USER_DEVSTACK=$(openssl rand -base64 12)
-    PASS_DEVSTACK=$(openssl rand -base64 12)
-    VERSION_TOOLCHAIN=$(openssl rand -base64 12)
     LD_LIBRARY_PATH="/usr/local/lib"
     function install_nvidia_repos() { echo "This would install_nvidia_repos ${*}"; }
     export -f install_nvidia_repos
@@ -92,10 +100,8 @@ function teardown() {
     export -f install_system_packages
     function install_nvidia_drivers() { echo "This would install_nvidia_drivers ${*}"; }
     export -f install_nvidia_drivers
-    function setup_xrdp() { echo "This would setup_xrdp ${*}"; }
-    export -f setup_xrdp
-    function install_vtd() { echo "This would install_vtd ${*}"; }
-    export -f install_vtd
+    function setup_x11vnc() { echo "This would setup_x11vnc ${*}"; }
+    export -f setup_x11vnc
     run run_main
     assert_success
 }
